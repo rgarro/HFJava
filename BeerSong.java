abbd126a9057000171abb12b606303695e71ee56
rgarro <rgarro@gmail.com>
2024-07-29 19:46:48 -0600

    jamaikina colombiana frapuchino

--- BeerSong.java
+++ BeerSong.java
@@ -5,6 +5,24 @@
  */
 public class BeerSong {
     public static void main(String[] args){
+        int beerNum = 88;
+        String word = "bottles";
+        while(beerNum > 0){
+            if(beerNum == 1){
+                word = "bottle";
+            }
 
+            System.out.println(String.valueOf(beerNum) + " " + word +" of beer on the wall");
+            System.out.println("Take One Down ...");
+            System.out.println("Pass it Around");
+
+            beerNum = beerNum - 1;
+
+            if(beerNum > 0){
+
+            }else{
+                System.out.println("THE END");
+            }
+        }
     }
 }
