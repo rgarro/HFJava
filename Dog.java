87eca96f70c50470badc130c21ca6e7af20896f0
rgarro <rgarro@gmail.com>
2024-07-30 18:29:35 -0600

    wa wa wa yipiyo yipiye ..

---
+++ Dog.java
@@ -0,0 +1,14 @@
+/*
+ * 
+ * 
+ *@author Rolando <rgarro@gmail.com> 
+ */
+public class Dog {
+    int size;
+    String breed;
+    String name;
+
+    void bark(){
+        System.out.println("Wa Wa Wa Yipiyo yipiye ..");
+    }
+}
