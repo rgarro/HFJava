878485cac1fcc3751219116fe7ca7f28b7d2fb6a
rgarro <rgarro@gmail.com>
2024-08-01 14:08:40 -0600

    java collectible heap memory

---
+++ Movie.java
@@ -0,0 +1,14 @@
+/*
+ * 
+ * 
+ *@author Rolando <rgarro@gmail.com> 
+ */
+public class Movie {
+    String title;
+    String genre;
+    int rating;
+
+    void playNow(){
+        System.out.println("Playing: " + this.title +" Genre:"+this.genre+" Rating: "+Integer.toString(this.rating));
+    }
+}
