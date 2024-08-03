efaca01c556aa3578879cf1e6ab051990fb225ee
rgarro <rgarro@gmail.com>
2024-08-03 17:42:04 -0600

    boolean expressions for our loops

---
+++ DotCom.java
@@ -0,0 +1,36 @@
+/*
+ * 
+ * 
+ *@author Rolando <rgarro@gmail.com> 
+ */
+import java.util.*;
+
+public class DotCom {
+
+    private ArrayList<String> locationCells;
+    private String name;
+
+    public void setLocationCells(ArrayList<String> loc){
+        this.locationCells = loc;
+    }
+
+    public void setName(String n){
+        this.name = n;
+    }
+
+    public String checkYourSelf(String userInput){
+        String result = "Miss";
+        int index = this.locationCells.indexOf(userInput);
+        if(index>=0){
+            this.locationCells.remove(index);
+            if(this.locationCells.isEmpty()){
+                result = "Kill";
+                System.out.println("ouch! you sunk: "+this.name);
+            }else{
+                result = "Hit";
+            }
+        }
+        return result;
+    }
+
+}
